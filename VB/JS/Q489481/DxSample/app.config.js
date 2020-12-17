923ac5f004d1c58c3ece4be6eb4e2d8313d1b3bb
DevExpressExampleBot <fakeEmail>
2020-12-17 13:54:37 +0300

    Source auto update [skip ci]

---
+++ VB/JS/Q489481/DxSample/app.config.js
@@ -0,0 +1,17 @@
+﻿window.DxSample = $.extend(true, window.DxSample, {
+    "config": {
+        "defaultLayout": "custom",
+        "navigation": [
+            {
+                title: "Index",
+                action: "#Index",
+                icon: "home"
+            },
+            {
+                title: "About",
+                action: "#About",
+                icon: "info"
+            }
+        ]
+    }
+});
